035f70569e63c585971ed631b5a7486acc6453cb
Master <masterdev41@gmail.com>
2025-06-29 17:18:39 +0300

    Bookings Page

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,8 +1,25 @@
+import { Button } from "../common/Button";
+import { Briefcase } from 'lucide-react';
+
 const Header = () => {
   return (
-    <header className="bg-blue-600 text-white py-4 px-8 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">Alx Listing App</h1>
-      <nav className="flex gap-4">
+    <header>
+      <div className="h-12 bg-primary text-white  flex justify-center items-center">
+        <div className="flex gap-4 items-center">
+          <Briefcase className="w-5 h-5" />
+          Overseas trip? Get the latest information on travel guides
+          <Button variant={"black_round"} size={"sm_round"}>More Info</Button>
+        </div>
+      </div>
+      <div className="mid-header-section border-b-gray-300 h-22 py-4 px-4  flex justify-between items-center">
+        <div className="logo">LOGO</div>
+        <div className="logo-center-section flex gap-2">Mid</div>
+        <div className="sign-in-section flex gap-4">
+          <Button variant={"primary_round"}>Sign In</Button>
+          <Button variant={"outline_round"}>Sign Up</Button>
+        </div>
+      </div>
+      {/* <nav className="flex gap-4">
         <a href="#" className="text-blue-500">
           Home
         </a>
@@ -12,7 +29,7 @@ const Header = () => {
         <a href="#" className="text-blue-500">
           Contact
         </a>
-      </nav>
+      </nav> */}
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/common/Button";
 import Layout from "@/components/layout/Layout";
 
 export default function Home() {
@@ -14,6 +15,9 @@ export default function Home() {
       <section>
         Listing Section
       </section>
+      <Button variant={"default"} className="bg-c-primary text-white">
+            Get Started
+          </Button>
     </>
   );
 }
